4e6b66b835ddcf7eeb9270ff336bfde94713f9af
IkhwalSyukur <ikhwalsyukur14@gmail.com>
2022-12-11 12:30:46 +0800

    tes vscode

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello World");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
